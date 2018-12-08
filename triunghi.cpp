90de51a8db0f494832de5c773638a7e800f8d171
Gheorghe Sarbu <ghesarbu@gmail.com>
2018-12-08 17:00:47 +0100

    Update triunghi.cpp

--- triunghi.cpp
+++ triunghi.cpp
@@ -1,11 +1,7 @@
 #include <iostream>
 using namespace std;
 
-//input: 4
-//output:  4  3  2  1
-//         3  2  1
-//         2  1
-//         1
+
 
 int main()
 {
@@ -24,11 +20,6 @@ int main()
               } 
    
     
-  system("PAUSE");//ca sa nu inchida fereastra cu output-ul
-  return 0;
+ return 0;
 }
 
-//for (i = 1; i <= N; ++i)
-//    cout<<i<<" ";//afiseaza numerele de la 1 la N
-//  for (i = N; i >= 1; --i)
-//    cout<<i<<" ";//afiseaza numerele de la N la 1
